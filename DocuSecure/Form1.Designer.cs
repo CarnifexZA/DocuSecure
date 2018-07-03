e2b111b3f334d411f0e499647db1b4e00157e247
CARNIFEXIUM\Carnifex <carnifex.za@gmail.com>
2018-07-03 23:33:15 +0200

    Initial Commit

---
+++ DocuSecure/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace DocuSecure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowseSourceFile = new System.Windows.Forms.Button();
+            this.txtSourceFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtFeedback = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutputDir = new System.Windows.Forms.TextBox();
+            this.btnBrowseOutputDir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEncryptedFile = new System.Windows.Forms.TextBox();
+            this.btnBrowseEncryptedFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowseSourceFile
+            // 
+            this.btnBrowseSourceFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseSourceFile.Location = new System.Drawing.Point(472, 20);
+            this.btnBrowseSourceFile.Name = "btnBrowseSourceFile";
+            this.btnBrowseSourceFile.Size = new System.Drawing.Size(29, 22);
+            this.btnBrowseSourceFile.TabIndex = 0;
+            this.btnBrowseSourceFile.Text = "...";
+            this.btnBrowseSourceFile.UseVisualStyleBackColor = true;
+            this.btnBrowseSourceFile.Click += new System.EventHandler(this.btnBrowseSourceFile_Click);
+            // 
+            // txtSourceFile
+            // 
+            this.txtSourceFile.Location = new System.Drawing.Point(128, 22);
+            this.txtSourceFile.Name = "txtSourceFile";
+            this.txtSourceFile.Size = new System.Drawing.Size(338, 20);
+            this.txtSourceFile.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Original Source File";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(507, 20);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "ENCRYPT";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtFeedback
+            // 
+            this.txtFeedback.Location = new System.Drawing.Point(27, 100);
+            this.txtFeedback.Multiline = true;
+            this.txtFeedback.Name = "txtFeedback";
+            this.txtFeedback.Size = new System.Drawing.Size(401, 278);
+            this.txtFeedback.TabIndex = 4;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(507, 72);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 8;
+            this.btnDecrypt.Text = "DECRYPT";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output File";
+            // 
+            // txtOutputDir
+            // 
+            this.txtOutputDir.Location = new System.Drawing.Point(128, 74);
+            this.txtOutputDir.Name = "txtOutputDir";
+            this.txtOutputDir.Size = new System.Drawing.Size(338, 20);
+            this.txtOutputDir.TabIndex = 6;
+            // 
+            // btnBrowseOutputDir
+            // 
+            this.btnBrowseOutputDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseOutputDir.Location = new System.Drawing.Point(472, 72);
+            this.btnBrowseOutputDir.Name = "btnBrowseOutputDir";
+            this.btnBrowseOutputDir.Size = new System.Drawing.Size(29, 22);
+            this.btnBrowseOutputDir.TabIndex = 5;
+            this.btnBrowseOutputDir.Text = "...";
+            this.btnBrowseOutputDir.UseVisualStyleBackColor = true;
+            this.btnBrowseOutputDir.Click += new System.EventHandler(this.btnBrowseOutputDir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Encrypted File";
+            // 
+            // txtEncryptedFile
+            // 
+            this.txtEncryptedFile.Location = new System.Drawing.Point(128, 48);
+            this.txtEncryptedFile.Name = "txtEncryptedFile";
+            this.txtEncryptedFile.Size = new System.Drawing.Size(338, 20);
+            this.txtEncryptedFile.TabIndex = 10;
+            // 
+            // btnBrowseEncryptedFile
+            // 
+            this.btnBrowseEncryptedFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseEncryptedFile.Location = new System.Drawing.Point(472, 46);
+            this.btnBrowseEncryptedFile.Name = "btnBrowseEncryptedFile";
+            this.btnBrowseEncryptedFile.Size = new System.Drawing.Size(29, 22);
+            this.btnBrowseEncryptedFile.TabIndex = 9;
+            this.btnBrowseEncryptedFile.Text = "...";
+            this.btnBrowseEncryptedFile.UseVisualStyleBackColor = true;
+            this.btnBrowseEncryptedFile.Click += new System.EventHandler(this.btnBrowseEncryptedFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 390);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEncryptedFile);
+            this.Controls.Add(this.btnBrowseEncryptedFile);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutputDir);
+            this.Controls.Add(this.btnBrowseOutputDir);
+            this.Controls.Add(this.txtFeedback);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSourceFile);
+            this.Controls.Add(this.btnBrowseSourceFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowseSourceFile;
+        private System.Windows.Forms.TextBox txtSourceFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtFeedback;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutputDir;
+        private System.Windows.Forms.Button btnBrowseOutputDir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEncryptedFile;
+        private System.Windows.Forms.Button btnBrowseEncryptedFile;
+    }
+}
+
---
+++ DocuSecure/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DocuSecure
+{
+    public partial class Form1 : Form
+    {
+
+        string mySourceFile = "";
+        string myEncryptedFile = "";
+        string myOutputFile = "";
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WriteToFeedback(string msg, bool newline)
+        {
+            try
+            {
+                if (newline)
+                {
+                    txtFeedback.AppendText(string.Format("{0}\n", msg));
+                }
+                else
+                {
+                    txtFeedback.AppendText(string.Format("{0}", msg));
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+
+        #region AES ENCRYPT / DECRYPT
+        // HELP MATERIAL - https://www.codeproject.com/Articles/769741/Csharp-AES-bits-Encryption-Library-with-Salt
+
+        private byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        private byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        private string EncryptText(string input, string password)
+        {
+            // Get the bytes of the string
+            byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            string result = Convert.ToBase64String(bytesEncrypted);
+
+            return result;
+        }
+
+        private string DecryptText(string input, string password)
+        {
+            // Get the bytes of the string
+            byte[] bytesToBeDecrypted = Convert.FromBase64String(input);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            string result = Encoding.UTF8.GetString(bytesDecrypted);
+
+            return result;
+        }
+
+        private void EncryptFile()
+        {
+            try
+            {
+                string password = "abcd1234";
+
+                byte[] bytesToBeEncrypted = File.ReadAllBytes(mySourceFile);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+                // Hash the password with SHA256
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+                //string fileEncrypted = "C:\\SampleFileEncrypted.DLL";
+
+                File.WriteAllBytes(myOutputFile, bytesEncrypted);
+
+                WriteToFeedback(string.Format("Encrypted File saved : {0}", myOutputFile), true);
+                WriteToFeedback("", true);
+            }
+            catch (Exception ex)
+            {
+                WriteToFeedback(string.Format("EXCEPTION : {0}", ex.ToString()), true);
+                WriteToFeedback("", true);
+            }
+        }
+
+        private void DecryptFile()
+        {
+            try
+            {
+                //string fileEncrypted = "C:\\SampleFileEncrypted.DLL";
+                string password = "abcd1234";
+
+                byte[] bytesToBeDecrypted = File.ReadAllBytes(myEncryptedFile);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+                //string file = "C:\\SampleFile.DLL";
+                File.WriteAllBytes(myOutputFile, bytesDecrypted);
+                WriteToFeedback(string.Format("Decrypted File saved : {0}", myOutputFile), true);
+                WriteToFeedback("", true);
+            }
+            catch (Exception ex)
+            {
+                WriteToFeedback(string.Format("EXCEPTION : {0}", ex.ToString()), true);
+                WriteToFeedback("", true);
+            }
+        }
+
+        #endregion
+
+
+
+        
+
+        private void btnBrowseSourceFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog opfd = new OpenFileDialog();
+                opfd.Filter = "Adobe PDF files (*.pdf)|*.pdf";
+                DialogResult result = opfd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    mySourceFile = opfd.FileName.ToString();
+                    WriteToFeedback(string.Format("File Selected : {0}", mySourceFile), true);
+                    WriteToFeedback("", true);
+                }
+                else
+                {
+                    WriteToFeedback("No file selected", true);
+                    WriteToFeedback("", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteToFeedback(string.Format("EXCEPTION : {0}", ex.ToString()), true);
+                WriteToFeedback("", true);
+            }
+        }
+
+        private void btnBrowseOutputDir_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    FolderBrowserDialog fbdl = new FolderBrowserDialog();
+            //    DialogResult result = fbdl.ShowDialog();
+
+            //    if (result == System.Windows.Forms.DialogResult.OK)
+            //    {
+            //        myOutputDir = fbdl.SelectedPath.ToString();
+            //        WriteToFeedback(string.Format("Output Directory : {0}", myOutputDir), true);
+            //        WriteToFeedback("", true);
+            //    }
+            //    else
+            //    {
+            //        WriteToFeedback("No Directory selected", true);
+            //        WriteToFeedback("", true);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+
+            try
+            {
+                SaveFileDialog sfdl = new SaveFileDialog();
+                sfdl.Filter = "Adobe PDF files (*.pdf)|*.pdf";
+                DialogResult result = sfdl.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    myOutputFile = sfdl.FileName.ToString();
+                    WriteToFeedback(string.Format("Output file selected : {0}", myOutputFile), true);
+                    WriteToFeedback("", true);
+                }
+                else
+                {
+                    WriteToFeedback("No Directory selected", true);
+                    WriteToFeedback("", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteToFeedback(string.Format("EXCEPTION : {0}", ex.ToString()), true);
+                WriteToFeedback("", true);
+            }
+        }
+
+        private void btnBrowseEncryptedFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog opfd = new OpenFileDialog();
+                opfd.Filter = "Adobe PDF files (*.pdf)|*.pdf";
+                DialogResult result = opfd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    myEncryptedFile = opfd.FileName.ToString();
+                    WriteToFeedback(string.Format("Encrypted File Selected : {0}", myEncryptedFile), true);
+                    WriteToFeedback("", true);
+                }
+                else
+                {
+                    WriteToFeedback("No Encrypted file selected", true);
+                    WriteToFeedback("", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteToFeedback(string.Format("EXCEPTION : {0}", ex.ToString()), true);
+                WriteToFeedback("", true);
+            }
+        }
+
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            EncryptFile();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            DecryptFile();
+        }
+
+        
+    }
+}
